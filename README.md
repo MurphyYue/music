9c6ea2cd86a1d45a8297ba946a3338b804958c0e
MurphyYue <1009098371@qq.com>
2017-04-13 09:03:39 +0800

    基本完善

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # music
-网易云音乐
-this is my object
-Hello my name is Murphy,this is my first code
-***javascript
-var a=1;
-for(var i=0;i<8;i++){
-	a++;
-}
-console.log(a)
+音乐webApp
+词音乐webAPP项目能做到正常播放音乐以及云端音乐库的请求加载，但是没有做到真正的跨域。在进入播放页面的时候，原接口用的是https，需要用到跨域，但是在此项目里并没有成功利用jsonP解决次跨域问题，而是通过追加一次ajax请求做到的把之前页面的图片加载到本页面。
 ***
--- object/js/app.js
+++ object/js/app.js
@@ -1,31 +1,82 @@
-
-function route(m,container){
-	container=container||$("#share")
+//加载模块的ajax函数
+function loadPage(page, container) {
+	container = container || $('#main');
+	var pageUrl = 'view/' + page + '.html';
 	$.ajax({
-		url:"views/"+m+".html",
-		success:function(data){
-			container.html(data);
-			console.log("ok")
-			loadJs(m);
-			
-		},
-		error:function(){
-			console.log("fail")
+		type: "get",
+		url: pageUrl,
+		success: function(data) {
+			container.html(data); // 刷新dom结构
+			loadJs(page); //加载对应dom的js
 		}
 	});
 }
-function loadJs(m){
-	
-	var jsUrl="js/"+m+".js"
+
+//图片未请求到之前执行的界面
+function loadImgAnimate(parent) {
 	$.ajax({
-		url:jsUrl,
-		dataType:"script"
-	})
+		url: "view/loadImganimate.html",
+		async: true,
+		success: function(data) {
+			$(data).appendTo(parent);
+		}
+	});
 }
-$(function(){
-	route("table");
-	route("play",$("#global"));
-	
+//加载对应的js
+function loadJs(page) {
+	var jsUrl = 'js/' + page + '.js';
+	if(page == 'build') return;
+
+	$.ajax({
+		url: jsUrl,
+		dataType: 'script'
+	});
+}
+//加载对应的模块
+$(function() {
+	//加载导航模块
+	loadPage('nav', $('#header'));
 
 })
-var myPlayList=[];
+//获得url参数
+function getUrlParams() {
+	var params = {};
+	var url = window.location.href;
+
+	var p = url.split("#");
+	if(p.length == 2)
+		p = p[1];
+	else
+		p = url;
+
+	p = p.split("?");
+	if(p.length < 2) {
+		params.anchor = p[0];
+		return params;
+	}
+
+	params.anchor = p[0];
+	p = p[1].split("&");
+
+	for(var i = 0; i < p.length; i++) {
+		var kv = p[i].split("=");
+		params[kv[0]] = kv[1];
+	}
+	return params;
+}
+
+//loadTopPage();
+////首次加载方法
+//function loadTopPage(){
+//	if(!localStorage.count){
+//		localStorage.count = 0;
+//	}
+//	localStorage.count++
+//	
+//	if(localStorage.count==1){
+//		loadPage('home');
+//	}else{
+//		//加载首页内容
+//		loadPage('hotSong');
+//	}
+//}
